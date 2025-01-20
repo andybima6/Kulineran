535b2aa36ebdead1208efc61891b6eee50e5193b
Andy Nugrahan Putra <andybima6@gmail.com>
2025-01-20 15:44:56 +0700

    feat: halaman food

--- src/views/FoodsView.vue
+++ src/views/FoodsView.vue
@@ -1,68 +1,100 @@
 <template>
-    <div>
-      <NavbarView />
-      <div class="container">
-        <HeroView />
-        <div class="row">
-          <div class="col mt-4">
-            <h2>Daftar <strong>Makanan</strong></h2>
-          </div>
+  <div>
+    <NavbarView />
+    <div class="container">
+      <HeroView />
+      <div class="row">
+        <div class="col mt-4">
+          <h2>Daftar <strong>Makanan</strong></h2>
         </div>
-  
-        <div class="row mb-3">
-          <!-- v-for adalah perulangan -->
-          <div class="col-md-3 mt-4" v-for="product in daftarProducts" :key="product.id">
-            <!-- Pass 'product' as the prop -->
-            <CardProduct :product="product" />
+      </div>
+
+      <div class="row mt-3">
+        <div class="col">
+          <div class="input-group mb-3">
+            <!--  v-model="search" isi harus sama dengan yang di return -->
+            <input 
+            v-model="search" 
+            @keyup="searchFood" 
+            type="text" 
+            class="form-control" 
+            placeholder="Cara Makanan Kesukaan Anda..." 
+            aria-label="Cara Makanan Kesukaan Anda..." 
+            aria-describedby="basic-addon1" />
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1"><b-icon-search></b-icon-search></span>
+            </div>
           </div>
         </div>
       </div>
+
+      <div class="row mb-3">
+        <!-- v-for adalah perulangan -->
+        <!-- kenapa untuk array bernama product dikarenakan product sudah dideklrasikan di cardview berupa data props -->
+        <div class="col-md-3 mt-4" v-for="product in daftarProducts" :key="product.id">
+          <!-- Pass 'product' as the prop -->
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import HeroView from "@/components/HeroView.vue";
-  import NavbarView from "@/components/Navbar.vue";
-  import CardProduct from "@/components/CardView.vue";
-  import axios from "axios";
-  
-  export default {
-    name: "FoodsView",
-    components: {
-      NavbarView,
-      HeroView,
-      CardProduct,
+  </div>
+</template>
+
+<script>
+import HeroView from "@/components/HeroView.vue";
+import NavbarView from "@/components/Navbar.vue";
+import CardProduct from "@/components/CardView.vue";
+import axios from "axios";
+
+export default {
+  name: "FoodsView",
+  components: {
+    NavbarView,
+    HeroView,
+    CardProduct,
+  },
+  data() {
+    return {
+      // Data property untuk menyimpan daftar produk
+      daftarProducts: [],
+      search: "",
+    };
+  },
+  methods: {
+    // Method untuk mengatur data 'daftarProducts'
+    setProduct(data) {
+      this.daftarProducts = data;
     },
-    data() {
-      return {
-        // Data property should be named 'daftarProducts'
-        daftarProducts: [],
-      };
-    },
-    methods: {
-      // Method to set 'daftarProducts'
-      setProduct(data) {
-        this.daftarProducts = data;
-      },
-    },
-    mounted() {
-      // Fetch data from API using Axios
+    // Method untuk mencari makanan
+    searchFood() {
       axios
-        .get("http://localhost:3000/products")
-        .then((response) =>
+        .get("http://localhost:3000/products?q=" + this.search)
+        .then((response) => {
           // Handle success
-          this.setProduct(response.data)
-        )
-        .catch((error) =>
+          this.setProduct(response.data);
+        })
+        .catch((error) => {
           // Handle error
-          console.log("error : ", error)
-        )
-        .finally(function () {
-          // Always executed
+          console.error("Error fetching products:", error);
         });
     },
-  };
-  </script>
-  
-  <style></style>
-  
+  },
+  mounted() {
+    // Fetch data dari API ketika komponen di-mount
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => {
+        // Handle success
+        this.setProduct(response.data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error("Error fetching products:", error);
+      });
+  },
+};
+</script>
+
+<style>
+/* Tambahkan style khusus untuk komponen ini jika diperlukan */
+</style>
